be10687ba3af5df9273c625e2517df92f50bf969
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2022-07-01 16:06:59 +0700

    Create Monkey.java

---
+++ CASE2/number2/Monkey.java
@@ -0,0 +1,13 @@
+package Number2;
+
+public class Monkey implements Animal{
+    private String name;
+    private int weight;
+    private String food;
+
+    public Monkey(String name, int weight, String food) {
+        this.name = name;
+        this.weight = weight;
+        this.food = food;
+    }
+}
