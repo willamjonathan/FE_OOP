0b3876e45182d3732ee79573f3b255dbb07e8df2
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2022-07-01 15:30:55 +0700

    Create Book.java

---
+++ CASE2/number1/Book.java
@@ -0,0 +1,48 @@
+public class Book {
+
+    //prvate variables
+    private String author;
+    private String title;
+    private double price;
+    private int year;
+    //public method
+    //constructors
+
+    public Book(String author, String title, double price, int year){
+        this.author = author;
+        this.title = title;
+        this.price = price;
+        this.year = year;
+    
+    }
+    //getter and setter methods
+    
+    public String getAuthor(){
+        return author;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+}
+
