456412d1fb0de85cc57e7d09e059a75b0955a23c
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2022-07-01 16:07:32 +0700

    Create Lion.java

---
+++ CASE2/number2/Lion.java
@@ -0,0 +1,13 @@
+package Number2;
+
+public class Lion implements Animal {
+    private String name;
+    private int weight;
+    private int meat;
+
+    public Lion(String name, int weight, int meat) {
+        this.name = name;
+        this.weight = weight;
+        this.meat = meat;
+    }
+}
