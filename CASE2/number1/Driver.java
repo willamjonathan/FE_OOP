13d13764698ea86321fe3b27cb1bb69f77490b25
William Jonathan Mulyadi <91537837+willamjonathan@users.noreply.github.com>
2022-07-01 15:34:57 +0700

    Update Driver.java

--- CASE2/number1/Driver.java
+++ CASE2/number1/Driver.java
@@ -7,11 +7,11 @@ public class Driver {
     Book b4 = new Book("Jude Martinez", "Bakemonogatari", 9.50, 1996);
 
     Book b5 = new Book("Ida Bagus","The Young Doctor",900,1974);
-
-    System.out.println(b1.getAuthor() +", "+ b1.getTitle()+", "+"$"+b1.getPrice()+", "+b1.getYear());
-    System.out.println(b2.getAuthor() +", "+ b2.getTitle()+", "+"$"+b2.getPrice()+", "+b2.getYear());
-    System.out.println(b3.getAuthor() +", "+ b3.getTitle()+", "+"$"+b3.getPrice()+", "+b3.getYear());
-    System.out.println(b4.getAuthor() +", "+ b4.getTitle()+", "+"$"+b4.getPrice()+", "+b4.getYear());
-    System.out.println(b5.getAuthor() +", "+ b5.getTitle()+", "+"$"+b5.getPrice()+", "+b5.getYear());
+    //printing the objects
+    System.out.println(b1.toString());
+    System.out.println(b2.toString());
+    System.out.println(b3.toString());
+    System.out.println(b4.toString());
+    System.out.println(b5.toString());
 }
 }
